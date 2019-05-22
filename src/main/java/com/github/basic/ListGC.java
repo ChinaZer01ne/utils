ee3c39c07de2f190ba79da4a83f6997d9ea07d27
lion <1148607441@qq.com>
2019-05-22 17:48:48 +0800

    practice"

---
+++ src/main/java/com/github/basic/ListGC.java
@@ -0,0 +1,9 @@
+package com.github.basic;
+
+/**
+ * @author Zer01ne
+ * @version 1.0
+ * @date 2019/5/21 16:19
+ */
+public class ListGC {
+}
---
+++ src/main/java/com/github/web/controller/RedisTestController.java
@@ -0,0 +1,9 @@
+package com.github.web.controller;
+
+/**
+ * @author Zer01ne
+ * @version 1.0
+ * @date 2019/5/21 11:34
+ */
+public class RedisTestController {
+}
