df6671eb3d2b17a43ad2df49ce043d41add0bfbf
peach <peach>
2019-06-11 00:06:58 +0800

    字符串转int

--- src/main/java/com/github/arithmetic/offer/把字符串转换成整数.java
+++ src/main/java/com/github/arithmetic/offer/把字符串转换成整数.java
@@ -31,23 +31,75 @@ package com.github.arithmetic.offer;
  */
 public class 把字符串转换成整数 {
 
-
+    public static void main(String[] args) {
+        把字符串转换成整数 a = new 把字符串转换成整数();
+        //System.out.println(a.StrToInt("+123"));
+        System.out.println(a.StrToInt("-2147483647"));
+        int i = -2147483648;
+    }
 
     /**
      *  0-9  对应ascii码  30-39
      * */
     public int StrToInt(String str) {
 
+        if ("-2147483648".equals(str)){
+            return Integer.MIN_VALUE;
+        }
+
         char[] charArray = str.toCharArray();
 
-        for (int i = 0; i < charArray.length; i++) {
-            if (charArray[i] < 30 && charArray[i] > 39){
+
+        boolean isNative = false;
+        boolean hasSymbol = false;
+        //判断第一个字母是不是符号
+        if (charArray.length > 0){
+            if ("+".equals(String.valueOf(charArray[0]))){
+                isNative = false;
+                hasSymbol = true;
+            }else if ("-".equals(String.valueOf(charArray[0]))){
+                isNative = true;
+                hasSymbol = true;
+            }else if (charArray[0] < 48 || charArray[0] > 57){
                 return 0;
             }
         }
 
+        //第一个字母之后，验证合法性
+        for (int i = 1; i < charArray.length; i++) {
+            if (charArray[i] < 48 || charArray[i] > 57){
+                return 0;
+            }
+        }
 
+        //字符串开头为0的情况
+        boolean isZero = true;
+        int cur = hasSymbol ? 1 : 0;
+        for (int i = 0; i < charArray.length; i++) {
+            if (charArray[i] == 0 && isZero){
+                cur++;
+                return 0;
+            }else {
+                isZero = false;
+            }
+        }
+        //进行加权运算
+        if (charArray.length - cur != 0){
 
+            int result = 0;
+            for (int i = cur; i < charArray.length; i++) {
+
+                //解决范围越界问题
+                if (result + (charArray[i] - 48) * Math.pow(10,charArray.length - 1 - cur) > Integer.MAX_VALUE){
+                    return 0;
+                }else {
+                    result += (charArray[i] - 48) * Math.pow(10,charArray.length - 1 - cur);
+                }
+                cur++;
+            }
+            return isNative ? -result : result;
+
+        }
 
         return 0;
     }
