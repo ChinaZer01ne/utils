4fe03c92a40ec112166e8deeb5b10b3c3e51d63a
lion <1148607441@qq.com>
2019-06-19 17:51:53 +0800

    mock

--- src/test/java/com/github/utils/Test.java
+++ src/test/java/com/github/utils/Test.java
@@ -1,21 +1,51 @@
 package com.github.utils;
 
+import com.github.GithubApplication;
+import com.github.GithubApplicationTests;
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 import org.yaml.snakeyaml.external.com.google.gdata.util.common.base.Escaper;
 import org.yaml.snakeyaml.external.com.google.gdata.util.common.base.UnicodeEscaper;
 
 import java.util.Random;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 /**
  * @Author: Zer01ne
  * @Date: 2018/12/20 11:43
  * @Version 1.0
  */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = GithubApplication.class)
 public class Test {
-    public static void main(String[] args) {
-        //Random random = new Random();
-        ////int s = random.nextInt(10) % (-1 +2 + 1) + 10;
-        //int s = random.nextInt(-10);
-        //System.out.println(s);
 
+    @Autowired
+    private WebApplicationContext wac;
+    private MockMvc mockMvc;
+    @Before
+    public void setup() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).build();   //构造MockMvc
+    }
+    @org.junit.Test
+    public void testIndex() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/index")
+                .accept(MediaType.APPLICATION_JSON).contentType(MediaType.APPLICATION_JSON)).andExpect(status().isOk());
+    }
+
+    @org.junit.Test
+    public void testHello() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.post("/hello")
+                .accept(MediaType.APPLICATION_JSON).contentType(MediaType.APPLICATION_JSON)).andExpect(status().isOk());
     }
 }
