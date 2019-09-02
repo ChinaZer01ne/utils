751b5c89a6656602162591cf1f3150ca0d3977c1
lion <1148607441@qq.com>
2019-09-02 17:40:05 +0800

    no message

--- src/test/java/com/github/GithubApplicationTests.java
+++ src/test/java/com/github/GithubApplicationTests.java
@@ -1,14 +1,26 @@
 package com.github;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.util.ClassUtils;
+import org.springframework.util.ResourceUtils;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 /**
  * @Author: Zer01ne
  * @Date: 2019/2/27 14:44
  * @Version 1.0
  */
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = GithubApplication.class)
 public class GithubApplicationTests {
     @Autowired
     private ThreadPoolTaskExecutor executor;
@@ -21,5 +33,16 @@ public class GithubApplicationTests {
             System.out.println("this is callable " +Thread.currentThread().getName() + " thread in thread pool.");
             return null;
         });
+    }
+
+
+    @Test
+    public void getFile() throws FileNotFoundException {
+        System.out.println(ClassUtils.getDefaultClassLoader().getResource("").getPath());
+        File file = Paths.get(ResourceUtils.getURL("classpath:").getPath().substring(1)).toFile();
+        for (File file1 : file.listFiles()) {
+            System.out.println(file1.getName());
+        }
+        //System.out.println(System.getProperty("user.dir"));
     }
 }
--- src/test/java/com/github/utils/jutil/DateUtilTest.java
+++ src/test/java/com/github/utils/jutil/DateUtilTest.java
@@ -3,6 +3,7 @@ package com.github.utils.jutil;
 import com.github.utils.jutils.DateUtil;
 import org.junit.Test;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.OffsetDateTime;
 import java.time.ZoneId;
@@ -31,5 +32,17 @@ public class DateUtilTest {
         System.out.println("String to Date : " + DateUtil.stringToDate("2019-01-01 00:00:00"));
         // Date to String
         System.out.println("String to Date : " + DateUtil.dateToString(new Date()));
+
+
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy/MM/dd");
+
+        LocalDate date = LocalDate.now();
+
+        String format = date.format(dateTimeFormatter);
+        System.out.println(format);
+        LocalDate parse = LocalDate.parse(format, dateTimeFormatter);
+        System.out.println(parse);
+
+
     }
 }
