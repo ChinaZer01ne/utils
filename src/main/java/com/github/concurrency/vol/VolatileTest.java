2629b64204673be9e755e5935011608612041432
lion <1148607441@qq.com>
2019-09-03 17:57:23 +0800

    no message

--- src/main/java/com/github/concurrency/vol/VolatileTest.java
+++ src/main/java/com/github/concurrency/vol/VolatileTest.java
@@ -1,16 +1,25 @@
 package com.github.concurrency.vol;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class VolatileTest {
 
-    private static boolean ready = true;
+    private static volatile boolean ready = true;
+    //private static boolean ready = true;
     private static int number;
+    private static int b;
 
     private static class ReaderThread extends Thread{
         @Override
         public void run() {
             while (ready){
-                System.out.println(number);
+                //System.out.println(number);
+                //int a = 1;
+                //b = a;
+                //b = 2;
             }
+            //System.out.println(number);
         }
     }
 
@@ -26,6 +35,9 @@ public class VolatileTest {
         number = 42;    //线程并没有打印42，而直接停止了说明，此处出现了指令重排
 
         ready = false;
+        //ready = !ready;
+
+        System.out.println(b);
     }
 
 }
